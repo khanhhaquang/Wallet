4a52a9e96d8368cf3080a271fe14f672e7b5d40d
DELL\admin <khanhhaquang@gmail.com>
2017-11-28 20:08:52 +0700

    finish draft

--- routes/apis/sendmoney.js
+++ routes/apis/sendmoney.js
@@ -4,40 +4,48 @@ var User = require('../../models/user');
 var Transaction = require('../../models/transaction');
 /* GET home page. */
 router.post('', function(req, res, next) {
-  User.findOne({ username: req.body.from_user }, function(error,doc){
+  // tìm và update người nhận
+  User.findOne({ username: req.body.to_user }, function(error,touser){
     if(error)
       res.send(error);
     else{
-      var money = doc.money - req.body.money;
-      doc.set({money: money });
-      doc.save(function (err, user) {
-        if (err) return res.send(err);
-      });
-    }
-  })
-  User.findOne({ username: req.body.to_user }, function(error,doc){
-    if(error)
-      res.send(error);
-    else{
-      var money = parseFloat(doc.money) + parseFloat(req.body.money);
-      console.log("new money of receiver "+ money);
-      doc.set({money: money });
-      doc.save(function (err, user) {
-        if (err) return res.send(err);
-      });
-    }
-  })
-  var newtransaction = {
-    from_user: req.body.from_user,
-    to_user: req.body.to_user,
-    money: req.body.money,
-    date:req.body.date
-  }
-  Transaction.create(newtransaction,function(error, newtransaction){
-    if(error)
-      res.send(error)
-    else{
-      res.json(newtransaction);
+      if(touser !== null){
+        var money = parseFloat(touser.money) + parseFloat(req.body.money);
+        console.log("new money of receiver "+ money);
+        touser.set({money: money });
+        touser.save(function (err, user) {
+          if (err) return res.send(err);
+        });
+        // update người gửi
+        User.findOne({ username: req.body.from_user }, function(error,fromuser){
+          if(error)
+            res.send(error);
+          else{
+            var money = fromuser.money - req.body.money;
+            fromuser.set({money: money });
+            fromuser.save(function (err, user) {
+              if (err) return res.send(err);
+            });
+          }
+        })
+        // update lịch sử hệ thống
+        var newtransaction = {
+          from_user: req.body.from_user,
+          to_user: req.body.to_user,
+          money: req.body.money,
+          date:req.body.date
+        }
+        Transaction.create(newtransaction,function(error, newtransaction){
+          if(error)
+            res.send(error)
+          else{
+            res.json(newtransaction);
+          }
+        })
+      }
+      else{
+        res.json("failed")
+      }
     }
   })
 });
